62182226de7367f78f80e42d10af368d61e1a1d9
Yashraj Jaiswal <yashraj6370@gmail.com>
2024-09-25 20:45:08 +0530

    revert quality gate changes

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,8 +1,5 @@
-def QualityGateStatus(SonarQubecredentialsId) {
-    timeout(time: 5, unit: 'MINUTES') {  // Adjust timeout as needed
-        def qg = waitForQualityGate() // This will wait for the analysis to finish and the status to be available
-        if (qg.status != 'OK') {
-            error "Pipeline aborted due to quality gate failure: ${qg.status}"
-        }
-    }
+def call(credentialsId){
+  waitForQualityGate abortPipeline: false, credentialsId: credentialsId
 }
+
+
