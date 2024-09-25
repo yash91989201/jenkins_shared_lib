92e0a2501dce64bf68fae0e8b61a04c15896b2d2
Yashraj Jaiswal <yashraj6370@gmail.com>
2024-09-25 19:34:42 +0530

    add timeout for quality gate status

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,9 +1,8 @@
-
-
-def call(credentialsId){
-
-waitForQualityGate abortPipeline: false, credentialsId: credentialsId
-
+def QualityGateStatus(SonarQubecredentialsId) {
+    timeout(time: 5, unit: 'MINUTES') {  // Adjust timeout as needed
+        def qg = waitForQualityGate() // This will wait for the analysis to finish and the status to be available
+        if (qg.status != 'OK') {
+            error "Pipeline aborted due to quality gate failure: ${qg.status}"
+        }
+    }
 }
-
-
